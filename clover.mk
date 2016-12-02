133740d65304c4dfdbc6fb7f78e86f32df1b411f
Ethan Chen <intervigil@gmail.com>
2016-12-02 02:48:41 -0500

    clover: Add assertions on TrustZone version
     * Only allow specific firmware version
    
    Change-Id: I1ae8c415028ba0b5e781f53c3a26225bcdc98100

--- clover.mk
+++ clover.mk
@@ -296,6 +296,10 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
+# Recovery
+PRODUCT_PACKAGES += \
+    librecovery_updater_clover
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
