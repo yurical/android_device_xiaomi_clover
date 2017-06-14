67c7a2bef73c6f149e5cdf888d6b4152130c062e
Vinay Verma <vvinay@codeaurora.org>
2017-06-14 14:35:18 +0530

    clover: Add vndk sp libraries to system lib.
    Change-Id: Ia5b0087506571a929169736dfe108e7ff3f6d40f

--- clover.mk
+++ clover.mk
@@ -339,3 +339,23 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
 	android.hardware.usb@1.0-service
+
+# VNDK
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0.vndk-sp \
+    android.hardware.graphics.allocator@2.0.vndk-sp \
+    android.hardware.graphics.mapper@2.0.vndk-sp \
+    android.hardware.graphics.common@1.0.vndk-sp \
+    android.hidl.base@1.0.vndk-sp \
+    libhwbinder.vndk-sp \
+    libbase.vndk-sp \
+    libcutils.vndk-sp \
+    libhardware.vndk-sp \
+    libhidlbase.vndk-sp \
+    libhidltransport.vndk-sp \
+    libutils.vndk-sp \
+    libc++.vndk-sp \
+    libsync.vndk-sp \
+    libbacktrace.vndk-sp \
+    libunwind.vndk-sp \
+    liblzma.vndk-sp
