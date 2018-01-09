4ac7fa6f609bd9e41cf21c1f66956cf18eace8b6
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-08 19:21:59 -0800

    clover: Build RCS and telephony-ext
    Change-Id: I916666e778340f77e48b9173a3fbcbd7d0c09239

--- clover.mk
+++ clover.mk
@@ -290,6 +290,17 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
+
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
