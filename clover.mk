3e5311ed34d53a3d5664d6c2dfa07507bcd94ff2
dianlujitao <dianlujitao@lineageos.org>
2018-02-20 08:47:00 -0600

    clover: Build LiveDisplay HAL
    Change-Id: I4bfac85c64599ced45983a86d98cfd810a97db99

--- clover.mk
+++ clover.mk
@@ -185,6 +185,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_clover
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_main.sh \
