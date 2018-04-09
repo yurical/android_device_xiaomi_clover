8bd606cd8af59d33daaeac61b6d2f1ec665dd29e
Rashed Abdel-Tawab <rashed@linux.com>
2018-04-08 17:03:34 -0700

    clover: Build a system version of android.hidl.base@1.0 for imsvt
    Change-Id: Icb911f414595117a28fb764e145fbbf6a7ee0787

--- clover.mk
+++ clover.mk
@@ -30,6 +30,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
 
