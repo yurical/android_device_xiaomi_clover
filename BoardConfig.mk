8c61a712c5d728b2a23ec7078422f3671d60f33c
Yurical <yurical1@outlook.com>
2018-08-10 22:13:35 +0900

    clover: Add initial common device tree files
    Change-Id: I650156a32a662ae24075a1caa63f0ead3ce49b09

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+-include vendor/xiaomi/clover/BoardConfigVendor.mk
---
+++ clover.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/xiaomi/clover/clover-vendor.mk)
