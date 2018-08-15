7a2b4084782fbd9b963776d2c7279d483fc83215
Yurical <yurical1@outlook.com>
2018-08-16 00:04:18 +0900

    clover: Add irqbalance configs from MIUI 8.9.3
    Change-Id: If7cea0f22ca41ff81342a82bcd0a052782101327

--- clover.mk
+++ clover.mk
@@ -172,6 +172,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# IRQ
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/irqbalance/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
+    $(LOCAL_PATH)/configs/irqbalance/msm_irqbalance_sdm630.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_sdm630.conf
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_main.sh \
