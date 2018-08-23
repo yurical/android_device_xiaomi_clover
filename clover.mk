ba84147422c06bd89b4989433ebb992ac32e07f9
Yurical <yurical1@outlook.com>
2018-08-23 21:38:08 +0900

    clover: Build graphics packages and dependencies
    Change-Id: I0115dd69ba4c55784751c3d14b792c0e4d3e3564

--- clover.mk
+++ clover.mk
@@ -124,6 +124,17 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
+# Graphics
+PRODUCT_PACKAGES += \
+    copybit.sdm660 \
+    hwcomposer.sdm660 \
+    gralloc.sdm660 \
+    libdisplayconfig \
+    liboverlay \
+    libqdMetaData.system \
+    libtinyxml \
+    memtrack.sdm660
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
