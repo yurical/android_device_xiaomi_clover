a7287053631671d4c897b3e415c5165d447e993a
Yurical <yurical1@outlook.com>
2018-08-25 18:46:49 +0900

    clover: Build libxml2
    Change-Id: I43e59ec0e0969713f2607144249237839ee62396

--- clover.mk
+++ clover.mk
@@ -81,6 +81,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    libxml2
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
