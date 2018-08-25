c6b2dec781bf2ee6fef9fd4cef50872b971d5add
Yurical <yurical1@outlook.com>
2018-08-25 18:39:56 +0900

    clover: Add HIDL ANT+ support
    Change-Id: Ibf967dc8ff32fed8a6c71dd4952aad699b720686

--- clover.mk
+++ clover.mk
@@ -34,6 +34,14 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 # Properties
 -include $(LOCAL_PATH)/vendor-props.mk
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library
+
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
