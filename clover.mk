6409e2fbe3edb0cd89dc79029137ddb6482bc79f
Yurical <yurical1@outlook.com>
2018-08-25 20:38:39 +0900

    clover: Build USB HIDL HAL interface
    Change-Id: I87011450be2bdf67f752eae70eb989c21305f3d8

--- clover.mk
+++ clover.mk
@@ -330,3 +330,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
+
+# USB
+PRODUCT_PACKAGES += \
+	android.hardware.usb@1.0-service
