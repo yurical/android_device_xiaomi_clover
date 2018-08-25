8e6ba3594a14a5d57d44ab411e3feb807ca9292e
Yurical <yurical1@outlook.com>
2018-08-25 19:12:42 +0900

    clover: gps: Clean up Android.mk
    Change-Id: I9219345f0e3b274210441be1bc44a2c9d54c6b96

--- gps/Android.mk
+++ gps/Android.mk
@@ -1,5 +1,7 @@
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-LOCAL_PATH := $(call my-dir)
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
+include $(call all-makefiles-under,$(call my-dir))
