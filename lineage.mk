76bcdf75044830a1274a5a5e8d91c789e0ecb6c5
Yurical <yurical1@outlook.com>
2018-09-04 21:51:51 +0900

    clover: Add build fingerprint from MIUI 8.9.20
    Change-Id: I27c8ec34e1840fc053827ac18f3ba205ded776ca

--- lineage.mk
+++ lineage.mk
@@ -16,7 +16,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI PAD 4
 PRODUCT_MANUFACTURER := Xiaomi
 
+BUILD_FINGERPRINT := "Xiaomi/clover/clover:8.1.0/OPM1.171019.019/8.9.20:user/release-keys"
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="clover-user 8.1.0 OPM1.171019.019 8.9.20 release-keys" \
     PRODUCT_NAME="clover" \
     TARGET_DEVICE="clover"
 
