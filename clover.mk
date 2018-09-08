e10c4a734646556683eee58677f738a1625969ca
dianlujitao <dianlujitao@lineageos.org>
2018-09-08 15:03:58 +0800

    clover: Drop android.hidl.manager@1.0.so
     * Unused.
    
    Change-Id: Ie6908cab8192fd1d54fa58a061d9de11807a1c18

--- clover.mk
+++ clover.mk
@@ -31,7 +31,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
 
 # Ramdisk
