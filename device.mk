323a3202ffba3efd4d9254b377d4b502e0629849
Yurical <yurical1@outlook.com>
2018-10-23 08:52:18 +0900

    clover: Remove IR service
    Change-Id: Ifa97f8357e2bfdebceb182d277b1dfd9743bfccb

--- device.mk
+++ device.mk
@@ -234,14 +234,6 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
-# Consumerir
-PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
-
 # Display
 PRODUCT_PACKAGES += \
     copybit.sdm660 \
