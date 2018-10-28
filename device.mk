5752b0064e1c1306316472ccd9407190e0d42c7e
Yurical <yurical1@outlook.com>
2018-10-28 14:13:20 +0900

    clover: Use QTI gatekeeper HAL
    Change-Id: I84b5ef7409c371613c66851038742e529f8ffb49

--- device.mk
+++ device.mk
@@ -250,11 +250,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
-# Gatekeeper HAL
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service
-
 # GPS
 PRODUCT_PACKAGES += \
     libcurl \
