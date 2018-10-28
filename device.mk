45bd3b350cc4d57a963a8ebb1d2ea4df8a00ad89
Yurical <yurical1@outlook.com>
2018-10-28 14:19:16 +0900

    clover: Use QTI keymaster HAL
    Change-Id: I886ab719db17ba4675ac038e45e92f6e6276af4c

--- device.mk
+++ device.mk
@@ -297,11 +297,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
-# Keymaster HAL
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
-
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_clover
